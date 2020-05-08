1a5ca09d52c96712d982442a32c806d6097431b0
Oscar L <58749849+olopez94@users.noreply.github.com>
2020-05-08 03:30:01 +0000

    Create (8 kyu) Convert a Number to a String.py

---
+++ (8 kyu) Convert a Number to a String/(8 kyu) Convert a Number to a String.py
@@ -0,0 +1,10 @@
+def number_to_string(num):
+    # Return a string of the number here!
+    return str(num)
+
+
+# def number_to_string(num):
+#    try:
+#        return str(num)
+#    except:
+#        return None
