d43797739b8c44537a6cbf105863dc4833c8cf19
Oscar L <58749849+olopez94@users.noreply.github.com>
2020-06-17 00:01:26 -0500

    Update (7 kyu) Descending Order.py

--- (7 kyu) Descending Order/(7 kyu) Descending Order.py
+++ (7 kyu) Descending Order/(7 kyu) Descending Order.py
@@ -4,3 +4,14 @@ def descending_order(num):
     listNum = sorted(listNum, reverse = True)
     ans = ''.join(listNum[:])
     return int(ans)
+'''
+def Descending_Order(num):
+    return int("".join(sorted(str(num), reverse=True)))
+
+def descending_order(num):
+    # Bust a move right here
+    sortNum = sorted(str(num), reverse = True)
+    ans = int(''.join(sortNum))
+    
+    return ans
+'''
